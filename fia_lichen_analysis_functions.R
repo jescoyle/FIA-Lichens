b06bbf0da8807bb7278de42d4425981e35edc140
Jes Coyle <jrcoyle@live.unc.edu>
2013-10-29 14:40:30 -0400

    Initial commit.

---
+++ fia_lichen_analysis_functions.R
@@ -0,0 +1,94 @@
+## This file contains functions used to analyze FIA lichen data.
+
+
+######################################################################################################
+### Functions
+
+# Two functions that calculate Spearman rank correlation coefficients between two sets of variables
+# and returns a dataframe with correlations or an array of permutation-based confidence intervals
+calc.corrs = function(predictors, responses, data){
+
+	richCors = sapply(predictors, function(x){
+		noNA = data[!is.na(data[,x]),]
+		Xvar = noNA[,x]
+		cor(Xvar, noNA[,responses], method="spearman")
+	})
+
+	richCors = data.frame(t(richCors))
+	names(richCors) = responses
+
+	richCors$predictor = rownames(richCors)
+	richCors$nplots = sapply(rownames(richCors), function(x) sum(!is.na(data[,x])))
+
+	return(richCors)
+}
+
+calc.corrs.null = function(predictors, responses, data, ITERATIONS=1000, intervals = c(0.025, 0.975)){
+	# Calculate null distribution quantiles of rho based on permutation
+	richCors.null = array(NA, dim=c(ITERATIONS, length(responses), length(predictors)),
+		dimnames=list(iteration=1:ITERATIONS, response=responses, predictor=predictors))
+	for(i in 1:ITERATIONS){
+		sapply(predictors, function(x){
+			noNA = data[!is.na(data[,x]),]
+			Xvar = noNA[,x]
+
+			Xvar = sample(Xvar)
+		
+			cor(Xvar, noNA[,responses], method="spearman")
+		}) -> richCors.null[i,,]
+	}
+	quant = apply(richCors.null, c(2,3), function(x) quantile(x, probs=intervals))
+
+	return(quant)
+}
+
+
+## A function that partitions variation between three models
+## Assumes same set of observations and same underlying model structure
+## For some reason this function isn't working. It returns the error that it can't find dataB.
+
+partvar3 = function(modlist){
+	#require(MuMIn)
+	modA = modlist[[1]]
+	modB = modlist[[2]]
+	modC = modlist[[3]]
+
+	dataA = as.matrix(modA$model)
+	response = dataA[,1]
+	dataA = dataA[,2:ncol(dataA)]
+	dataB = as.matrix(modB$model)
+	dataB = dataB[,2:ncol(dataB)]	
+	dataC = as.matrix(modC$model)
+	dataC = dataC[,2:ncol(dataC)]
+
+	modAB = update(modA, ~.+dataB)
+	modAC = update(modA, ~.+dataC)
+	modBC = update(modB, ~.+dataC)
+	modABC = update(modAB, ~.+dataC)
+
+	mods = list(modA, modB, modC, modAB, modAC, modBC, modABC)
+	r2s = sapply(mods, r.squaredLR)
+
+	components = r2s[7]-r2s[6:4]
+	components = c(components, -(r2s[4]-r2s[1]-r2s[2]),
+		-(r2s[6]-r2s[2]-r2s[3]), -(r2s[5]-r2s[1]-r2s[3]))
+	components = c(components, -(r2s[7]-sum(components[1:6]))/2)
+	components[4:6] = components[4:6]-components[7]
+	
+	names(components) = c('A','B','C','AplusB','BplusC','AplusC','all')
+	
+	return(components)
+}
+
+
+# A function which plots any type of polynomial given the coefficients.
+#  a is a vector of coefficients
+polyfunc = function(x,a){
+	series = c()
+	
+	for(p in 1:length(a)){
+		series = cbind(series, a[p]*(x^(p-1)))
+	}
+
+	rowSums(series)
+}
---
+++ make_data_tables_for_analysis.R
@@ -0,0 +1,316 @@
+## This script compiles data tables for FIA lichen plots into:
+## master : a data table with all variables from 2228 plots
+## model_data : untransformed environmental data used in models from all plots without any data missing, includes PCA variables
+## working_data_unstd :  environmental data log or sqrt transformed to reduce skew and scaled by linear factor (usually 10) to put variables on similar range
+## working_data : working_data_unstd scaled to have mean 0 and std dev 1
+## Outliers are analyzed and removed from all data sets and working data sets are divided into equal sized test ('_test') and fitting ('_fit') sets.
+
+
+setwd('C://Users/jrcoyle/Documents/Projects/FIA Lichen')
+options(stringsAsFactors=F)
+varnames=read.csv('varnames.csv', row.names=1)
+source('fia_lichen_analysis_functions.R')
+
+########################################################################
+### Combine data files into a master data file and working data file ###
+
+# FIA plot data
+plot_locs = read.csv('./Data/fia_lichen_plot_locations.csv')
+county_data = read.csv('./Data/fia_lichen_county_data.csv')
+plot_data = merge(plot_locs, county_data[,c('COUNTYNM','state.abbr','STATE','COUNTY','POP2000','POP2010','SQMI')])
+
+# Lichen richness data
+rich_current = read.csv('./Data/lichen_richness_current.csv')
+rich_legacy = read.csv('./Data/lichen_richness_legacy.csv')
+rich_current = rich_current[,names(rich_legacy)]
+rich_data = rbind(rich_current, rich_legacy)
+
+# Lichen abundance data
+abun_data = read.csv('./Data/lichen abundance on plots.csv') # Not available for all plots b/c originally calculated after subsetting.
+
+# FIA tree data
+tree_data = read.csv('./Data/TreeData/master_data_forest.csv')[,c('yrplot.id','S.tree','D.abun.tree',
+	'D.area.tree','maxDiam','numTreesBig','numTreesSm','propDead','numDead','numCut',
+	'PIE.stems.tree','PIE.ba.tree','wood_moist_pct.rao.pres','bark_moist_pct.rao.pres',
+	'wood_SG.rao.pres','bark_SG.rao.pres','LogSeed.rao.pres','wood_moist_pct.rao.stems',
+	'bark_moist_pct.rao.stems','wood_SG.rao.stems','bark_SG.rao.stems','LogSeed.rao.stems',
+	'wood_moist_pct.stems','bark_moist_pct.stems','wood_SG.stems','bark_SG.stems','LogSeed.stems',
+	'wood_moist_pct.rao.ba','bark_moist_pct.rao.ba','wood_SG.rao.ba','bark_SG.rao.ba','LogSeed.rao.ba',
+	'wood_moist_pct.ba','bark_moist_pct.ba','wood_SG.ba','bark_SG.ba','LogSeed.ba','diamDist.mean',
+	'n.stems','basal.area','light.mean','light.var','lightDist.mean','totalCirc')]
+
+# Lichen functional diversity data
+fd_data = read.csv('./Data/LichenTraits/fia_lichen_LIAS_means_diversity.csv')
+
+# Lichen regional richness data
+reg_data = read.csv('./Data/Regional Richness/fia_lichen_reg_richness.csv')
+
+# Environmental data
+env_data = read.csv('./Data/fia_lichen_env_data_points.csv')
+env_plot_data = read.csv('./Data/fia_lichen_env_data_plots.csv')
+
+# Merge to make master data file
+master = merge(plot_data, env_plot_data, all.x=T, all.y=F)
+master = merge(master, rich_data, all.x=T, all.y=F)
+master = merge(master, abun_data, all.x=T, all.y=T)
+master = merge(master, tree_data, all.x=T, all.y=F)
+master = merge(master, fd_data, all.x=T, all.y=F)
+master = merge(master, reg_data, all.x=T, all.y=F)
+master = merge(master, env_data, all.x=T, all.y=F)
+
+# Save data
+write.csv(master, './Data/fia_lichen_master_data.csv', row.names=F)
+
+rownames(master) = master$yrplot.id
+###############################################################################
+### Data Subsetting ###
+
+# Use recent plots after plot design had been standardized
+model_data = subset(master, YEAR>=1997)
+
+# Set row names to identify plots
+rownames(model_data) = model_data$yrplot.id
+
+# Remove plots with only one large tree (heterogeneity measurements are NA)
+model_data = subset(model_data, numTreesBig>1) # removes 44 plots widely distributed across US
+
+## Define variables used in analysis
+# Make a list of predictors of each type
+climate = c('ap','mat','iso','pseas','rh')
+pollution = c('totalNS')
+forest_het = c('bark_SG.rao.ba', 'bark_moist_pct.rao.ba', 'wood_SG.rao.ba', 'wood_moist_pct.rao.ba', 
+	'LogSeed.rao.ba','lightDist.mean','PIE.ba.tree','S.tree', 'propDead', 'diamDist.mean')
+forest_hab = c('bark_SG.ba', 'bark_moist_pct.ba', 'wood_SG.ba', 'wood_moist_pct.ba', 
+	'LogSeed.ba','light.mean','totalCirc')
+forest_time = c('maxDiam')
+region = c('regS')
+
+predictors = data.frame(pred = c(climate, pollution, forest_het, forest_hab, forest_time, region),
+	type = c(rep('climate',length(climate)+length(pollution)),rep('forest',length(forest_het)+length(forest_hab)+length(forest_time)), rep('region',length(region))),
+	shape = c(rep(2,length(climate)), rep(1, length(pollution)), rep(1, length(forest_het)), rep(2, length(forest_hab)), rep(1, length(forest_time)), rep(1,length(region))),
+	hyp = c(rep('resource',length(climate)+length(pollution)), rep('niche', length(forest_het)), rep('resource', length(forest_hab)), rep('time', length(forest_time)), rep('region', length(region)))
+)
+predictors[predictors$pred=='totalCirc','shape']<-1
+
+# Plot histograms of predictors
+pdf('./Figures/Predictor variable histograms.pdf', height=6, width=6)
+for(p in predictors$pred){
+	hist(model_data[,p], main=varnames[p,'displayName'])
+	mtext(paste('# Missing =',sum(is.na(model_data[,p]))), side=3, line=0, adj=1)
+}
+dev.off()
+
+# Remove records that are missing data in these variables
+model_data = model_data[rowSums(is.na(model_data[,predictors$pred]))==0,]
+
+## Test for correlations among variables
+
+# Standardize variables
+use_response = 'lichen.rich'
+use_pred = predictors$pred
+use_data = model_data[,c(use_response,use_pred)]
+
+# Transform skewed variables (except proportions)
+logTrans_vars = c('totalCirc', 'ap')
+sqrtTrans_vars = c('bark_SG.rao.ba','bark_moist_pct.rao.ba','wood_SG.rao.ba',
+	'wood_moist_pct.rao.ba','LogSeed.rao.ba', 'S.tree')
+for(v in logTrans_vars){
+	use_data[,v] = log10(use_data[,v])
+}
+for(v in sqrtTrans_vars){
+	use_data[,v] = sqrt(use_data[,v])
+}
+# Center and scale data
+use_data[,2:ncol(use_data)] = scale(use_data[,2:ncol(use_data)] )
+
+# Pairwise correlations
+library('corrplot')
+
+# Record correlations between variables to determine whether to get rid of some.
+cortab = cor(use_data, use='complete.obs')
+which(cortab>0.7&cortab<1, arr.ind=T) # rh:ap, PIE.ba.tree:S.tree:bark_SG.rao.ba:LogSeed.rao.ba, maxDiam:diamDist.mean, LogSeed.ba:wood_SG.ba
+write.csv(cortab, 'correlation matrix std vars.csv', row.names=T)
+
+cortabsig = 1-abs(cortab)
+
+png('./Figures/New Coordinates/correlation matrix std vars.png', height=900, width=900, type='cairo')
+corrplot(cortab[2:nrow(cortab),2:ncol(cortab)], method='square', type='upper', diag=F, 
+	order='original', hclust.method='complete', p.mat=cortabsig[2:nrow(cortab),2:ncol(cortab)],
+	sig.level=0.6, insig='blank', tl.cex=1.5, tl.col=1, cl.cex=2, mar=c(1,1,4,1))
+dev.off()
+
+## Define new PCA variable pairs for dependently correlated variables
+newvars = data.frame(yrplot.id=rownames(model_data))
+rownames(newvars)==rownames(model_data)
+
+# max tree size and tree size range
+diam_pca = prcomp(na.omit(model_data[,c('diamDist.mean','maxDiam')]))
+diam_vars = data.frame(predict(diam_pca))
+names(diam_vars) = c('bigTrees','diamDiversity')
+diam_vars$diamDiversity = -1*diam_vars$diamDiversity
+diam_vars$yrplot.id = rownames(diam_vars)
+newvars = merge(newvars, diam_vars, all.x=T)
+
+# precipitation and humidity
+precip_pca = prcomp(use_data[,c('rh','ap')])
+precip_vars = data.frame(predict(precip_pca))
+names(precip_vars) = c('wetness','rain_lowRH')
+newvars = cbind(newvars, precip_vars)
+
+## Create data set with variables used for modeling
+myvars = c('lichen.rich','mat','iso','pseas','totalNS',
+	'bark_moist_pct.ba','bark_moist_pct.rao.ba','wood_SG.ba','wood_SG.rao.ba',
+	'LogSeed.ba','LogSeed.rao.ba','PIE.ba.tree','propDead','light.mean','lightDist.mean',
+	'totalCirc','regS','regParm','regPhys','tot_abun_log','parm_abun_log','phys_abun_log'
+)
+
+model_data = cbind(model_data[,myvars], newvars[,2:ncol(newvars)])
+
+## Create scaled and transformed datasets
+t(apply(model_data, 2, range)) # Examine ranges of variables
+
+trans_data = model_data
+logTrans_vars = c('totalCirc')
+sqrtTrans_vars = c('bark_moist_pct.rao.ba','wood_SG.rao.ba','LogSeed.rao.ba')
+for(v in logTrans_vars){
+	trans_data[,v] = log10(trans_data[,v])
+}
+for(v in sqrtTrans_vars){
+	trans_data[,v] = sqrt(trans_data[,v])
+}
+hist(trans_data$bigTrees) # Not much I can do about transforming this, so I won't
+
+# Rescale by constant so that variances in path analysis will be of similar scale
+working_data_unstd = trans_data
+working_data_unstd$mat = working_data_unstd$mat/10
+working_data_unstd$pseas = working_data_unstd$pseas/10
+working_data_unstd$totalNS = working_data_unstd$totalNS/100
+working_data_unstd$bark_moist_pct.ba = working_data_unstd$bark_moist_pct.ba/10
+working_data_unstd$bark_moist_pct.rao.ba = working_data_unstd$bark_moist_pct.rao.ba*10
+working_data_unstd$wood_SG.rao.ba = working_data_unstd$wood_SG.rao.ba*10
+working_data_unstd$wood_SG.ba = working_data_unstd$wood_SG.ba*10
+working_data_unstd$LogSeed.rao.ba = working_data_unstd$LogSeed.rao.ba*10
+working_data_unstd$PIE.ba.tree = working_data_unstd$PIE.ba.tree*10
+working_data_unstd$propDead = working_data_unstd$propDead*10
+working_data_unstd$light.mean = working_data_unstd$light.mean/10
+working_data_unstd$lightDist.mean = working_data_unstd$lightDist.mean/10
+working_data_unstd$regS = working_data_unstd$regS/10
+working_data_unstd$regParm = working_data_unstd$regParm/10
+working_data_unstd$regPhys = working_data_unstd$regPhys/10
+working_data_unstd$bigTrees = working_data_unstd$bigTrees/10
+
+# Rescale by mean and stddev
+working_data = trans_data
+working_data[,2:ncol(working_data)] = scale(working_data[,2:ncol(working_data)])
+
+# Make transformation of richness response used in models
+working_data$lichen.rich_log = log(working_data$lichen.rich+1)
+working_data_unstd$lichen.rich_log = log(working_data_unstd$lichen.rich+1)
+#working_data_unstd$lichen.rich = working_data_unstd$lichen.rich/10
+
+# Plot correlation matrix of rescaled and transformed data
+cortab = cor(working_data_unstd, use='complete.obs')
+cortabsig = 1-abs(cortab)
+
+png('./Figures/New Coordinates/correlation matrix unstd working vars.png', height=900, width=900, type='cairo')
+corrplot(cortab[2:nrow(cortab),2:ncol(cortab)], method='square', type='upper', diag=F, 
+	order='hclust', hclust.method='complete', p.mat=cortabsig[2:nrow(cortab),2:ncol(cortab)],
+	sig.level=0.6, insig='blank', tl.cex=1.5, tl.col=1, cl.cex=2, mar=c(1,1,4,1))
+dev.off()
+
+## Determine which points are outliers and remove.
+
+#	bark_moist_pct.rao.ba, wood_SG.ba, 
+#	wood_SG.rao.ba, LogSeed.rao.ba, propDead, light.mean, 
+#	lightDist.mean, diamDiversity, bigTrees, totalCirc
+
+plot(working_data$bark_moist_pct.rao.ba~rank(working_data$bark_moist_pct.rao.ba))
+
+outliers = working_data_unstd[,2:ncol(working_data_unstd)]
+outliers[,]<-NA
+for(v in names(working_data_unstd)[-1]){
+	ols = lm(log(working_data_unstd$lichen.rich+1)~working_data_unstd[,v])
+	cd = cooks.distance(ols)
+	outs = which(cd >4/nrow(working_data))
+
+	outliers[outs,v]<-cd[outs]
+}
+outliers = outliers[apply(outliers, 1, function(x) sum(!is.na(x))>0),]
+outliers$numOut = apply(outliers, 1, function(x) sum(!is.na(x)))
+write.csv(outliers, 'cooks D outliers.csv', row.names=T)
+
+# All 35 plots with 1 species are outliers
+rownames(subset(model_data, lichen.rich==1)) %in% rownames(outliers)[which(outliers$numOut>12)]
+outliers[rownames(subset(model_data, lichen.rich==1)),]
+
+# Only 5 plots with more that 2 species are outliers in 1/4 of the predictor variables.
+subset(model_data, rownames(model_data) %in% rownames(outliers)[which(outliers$numOut>7)])
+
+# Used to check outliers in each variable
+i=26
+ols = lm(working_data_unstd$lichen.rich_log~working_data_unstd[,i])
+opar <- par(mfrow = c(2, 2), oma = c(0, 0, 1.1, 0))
+plot(ols, las = 1)
+cd = cooks.distance(ols)
+which(cd > 4/nrow(working_data_unstd))
+outliers[order(outliers[,(i-1)], decreasing=T),][1:20,]
+subset(model_data, rownames(model_data) %in% names(which(cd>0.01)))
+
+# mat - none
+# iso - none
+# pseas - none
+# totalNS - none
+# bark_moist_pct.ba - none
+# bark_moist_pct.rao.ba: 1998_17_43_6379
+# wood_SG - none
+# wood_SG.rao.ba - none
+# LogSeed.ba : 1999_41_25_7306
+# LogSeed.rao.ba - none
+# PIE.ba.tree - none
+# propDead - none
+# light.mean - none: 1997_56_7_6475, 2007_4_9_87353 are outliers with 2-3 trees
+# lightDist.mean : 2007_4_19_83376
+# totalCirc - none
+# regS - none
+# regParm - none
+# regPhys - none
+# tot_abun_log : several appear to be outlier b/c there is one species
+# parm_abun_log - none
+# phys_abun_log - none
+# bigTrees - none
+# diamDiversity : 2004_16_49_85627, two trees diams 5.5, 27.1 leads to large diameter difference for relatively small trees, PCA exacerbates this
+# wetness - none
+# rain_lowRH - none
+
+# Remove outliers
+working_data = subset(working_data, !(rownames(working_data) %in% c('2004_16_49_85627','2007_4_19_83376','1999_41_25_7306','1998_17_43_6379')))
+working_data_unstd = subset(working_data_unstd, !(rownames(working_data_unstd) %in% c('2004_16_49_85627','2007_4_19_83376','1999_41_25_7306','1998_17_43_6379')))
+model_data = subset(model_data, !(rownames(model_data) %in% c('2004_16_49_85627','2007_4_19_83376','1999_41_25_7306','1998_17_43_6379')))
+trans_data = subset(trans_data, !(rownames(trans_data) %in% c('2004_16_49_85627','2007_4_19_83376','1999_41_25_7306','1998_17_43_6379')))
+
+## Divide data into fitting and testing data sets
+rownames(master) = master$yrplot.id
+allplots = rownames(model_data)
+usedata = master[allplots, c('state.abbr', 'yrplot.id')]
+
+# Only run once !!! 
+#unlist(tapply(usedata$yrplot.id, usedata$state.abbr, function(x){
+#	n = ifelse(runif(1)>0.5, ceiling(length(x)/2), floor(length(x)/2))
+#	sample(x, n)
+#}))->fitplots
+#length(fitplots) # stopped at 961
+#testplots = allplots[!(allplots %in% fitplots)]
+
+working_data_test = working_data[testplots,]
+working_data_fit = working_data[fitplots,]
+working_data_unstd_test = working_data_unstd[testplots,]
+working_data_unstd_fit = working_data_unstd[fitplots,]
+
+# Write out list of test and fit plots
+write.csv(data.frame(yrplot.id=testplots), './Data/model test plots.csv')
+write.csv(data.frame(yrplot.id=fitplots), './Data/model fit plots.csv')
+
+
+
+
+
