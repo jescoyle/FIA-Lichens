cc9eb9f665772ca573e62843fc03d9f9d203b3a4
Jes Coyle <jrcoyle@live.unc.edu>
2014-01-31 08:50:38 -0500

    moved cluster scripts to new folder

--- kure_sem_boot_soil.R
+++
@@ -1,9 +0,0 @@
-# This script runs bootstrap parameter estimation for the Soil SEM model
-
-library(lavaan,  lib.loc="/nas02/home/j/r/jrcoyle/Rlibs/")
-
-load('soil_model.Rdata')
-
-endfit_std = bootstrapLavaan(endfit, R=10000, FUN=function(x) c(parameterEstimates(x)$est,standardizedSolution(x)$est.std))
-
-save.image('soil_model.Rdata')
